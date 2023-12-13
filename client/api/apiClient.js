3beb5728e39c5543f50ac242f1c2712905a5ee81
Sue Raisianzadeh <raisianz@gmail.com>
2023-12-14 11:05:50 +1300

    removing extra code- new api

--- client/api/apiClient.js
+++ client/api/apiClient.js
@@ -1,59 +1,30 @@
-// client/api/apiClient.js
 import request from 'superagent'
 
-const apiBaseUrl = 'https://www.thesportsdb.com/api/v1/json/3/all_sports.php'
+const apiBaseUrl =
+  'https://allsportsapi2.p.rapidapi.com/api/placeholder/player.png'
 
-export async function getAllSports() {
-  try {
-    const response = await request.get(
-      `https://www.thesportsdb.com/api/v1/json/3/all_sports.php/all_sports.php`
-    )
-    return response.body.sports
-  } catch (error) {
-    console.error('Error fetching all sports:', error)
-    throw error
-  }
+const headers = {
+  'X-RapidAPI-Key': '4540e7eef7msh137d391951dcb69p11f962jsn3eafe93a6097',
+  'X-RapidAPI-Host': 'allsportsapi2.p.rapidapi.com',
 }
+
 export async function getPopularSports() {
-  const response = await request.get(
-    `https://www.thesportsdb.com/api/v1/json/3/all_sports.php/all_sports.php`
-  )
-  return response.body.sports
-}
-export async function getSportByCategory(categoryName) {
-  const response = await request.get(
-    `https://www.thesportsdb.com/api/v1/json/3/all_category.php`
-  )
-  return response.body.sports
+  const response = await request
+    .get(`${apiBaseUrl}/all_leagues.php`)
+    .set(headers)
+  return response.body.leagues // Assuming 'leagues' represent popular sports
 }
 
-export async function getAllLeagues() {
-  const response = await request.get(
-    `$https://www.thesportsdb.com/api/v1/json/3/all_leagues.php`
-  )
-  return response.body.leagues
+export async function getSportDetailsById(id) {
+  const response = await request
+    .get(`${apiBaseUrl}/lookupsport.php?id=${id}`)
+    .set(headers)
+  return response.body.sports[0]
 }
 
-export async function getLeagueDetailsById(leagueId) {
-  const response = await request.get(
-    `$thesportsdb.com/api/v1/json/all_countries.php
-    id=${leagueId}`
-  )
-  return response.body.leagues[0]
-}
-
-export async function getTeamsByLeagueName(leagueName) {
-  const response = await request.get(
-    `https://www.thesportsdb.com/api/v1/json/3/all_sports.php/search_all_teams.php?l=${encodeURIComponent(
-      leagueName
-    )}`
-  )
+export async function getTeamsByLeagueId(leagueId) {
+  const response = await request
+    .get(`${apiBaseUrl}/lookup_all_teams.php?id=${leagueId}`)
+    .set(headers)
   return response.body.teams
-}
-
-export async function getTeamDetailsById(teamId) {
-  const response = await request.get(
-    `$https://www.thesportsdb.com/api/v1/json/3/all_sports.php/lookupteam.php?id=${teamId}`
-  )
-  return response.body.teams[0]
 }
