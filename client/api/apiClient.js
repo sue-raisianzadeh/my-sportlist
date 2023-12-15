2d9ecae390bd8725e1a6c536e56b60f423b8b536
Sue Raisianzadeh <raisianz@gmail.com>
2023-12-15 15:57:49 +1300

    using rapid api

--- client/api/apiClient.js
+++ client/api/apiClient.js
@@ -1,59 +1,33 @@
-// client/api/apiClient.js
-import request from 'superagent'
+import axios from 'axios'
 
-const apiBaseUrl = 'https://www.thesportsdb.com/api/v1/json/3/all_sports.php'
+// Replace 'YOUR_RAPIDAPI_KEY' with your actual RapidAPI key
+const rapidAPIKey = 'APIKey: b553c64367msh2737608445cc59dp1cdd54jsnfbe2fc0027dd'
 
-export async function getAllSports() {
+// Setting up the axios instance with RapidAPI host and key
+const rapidAPI = axios.create({
+  baseURL: 'https://api-sports.api-host.com/',
+  headers: {
+    'x-rapidapi-key': rapidAPIKey,
+    'x-rapidapi-host': 'API_HOST',
+  },
+})
+
+export const getPopularSports = async () => {
   try {
-    const response = await request.get(
-      `https://www.thesportsdb.com/api/v1/json/3/all_sports.php/all_sports.php`
-    )
-    return response.body.sports
+    const response = await rapidAPI.get('/sports')
+    return response.data
   } catch (error) {
-    console.error('Error fetching all sports:', error)
+    console.error('Error fetching sports:', error)
     throw error
   }
 }
-export async function getPopularSports() {
-  const response = await request.get(
-    `https://www.thesportsdb.com/api/v1/json/3/all_sports.php/all_sports.php`
-  )
-  return response.body.sports
-}
-export async function getSportByCategory(categoryName) {
-  const response = await request.get(
-    `https://www.thesportsdb.com/api/v1/json/3/all_category.php`
-  )
-  return response.body.sports
-}
 
-export async function getAllLeagues() {
-  const response = await request.get(
-    `$https://www.thesportsdb.com/api/v1/json/3/all_leagues.php`
-  )
-  return response.body.leagues
-}
-
-export async function getLeagueDetailsById(leagueId) {
-  const response = await request.get(
-    `$thesportsdb.com/api/v1/json/all_countries.php
-    id=${leagueId}`
-  )
-  return response.body.leagues[0]
-}
-
-export async function getTeamsByLeagueName(leagueName) {
-  const response = await request.get(
-    `https://www.thesportsdb.com/api/v1/json/3/all_sports.php/search_all_teams.php?l=${encodeURIComponent(
-      leagueName
-    )}`
-  )
-  return response.body.teams
-}
-
-export async function getTeamDetailsById(teamId) {
-  const response = await request.get(
-    `$https://www.thesportsdb.com/api/v1/json/3/all_sports.php/lookupteam.php?id=${teamId}`
-  )
-  return response.body.teams[0]
+export const getSportDetailsById = async (id) => {
+  try {
+    const response = await rapidAPI.get(`/sports/${id}`)
+    return response.data
+  } catch (error) {
+    console.error('Error fetching sport details:', error)
+    throw error
+  }
 }
